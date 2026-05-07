f1c111134672f9dec4b74a732088a5913c849531
Connor <connor.lepre@gmail.com>
2026-05-07 16:50:07 -0400

    Sky layer exists, piano roll -> Main
    - Skylayer is a new node in Main that the Piano Roll has been parented to. This way, when the SkyLayer rotates, the piano roll and solution key will rotate with it out of frame.
    - Fixed a bug, missing signal connection for focus_exited on the BPM field in dev tools.
    - Adjusted main and sequencer scripts to move/replace references
    - Added public method to get BPM from the sequencer

--- core/main.gd
+++ core/main.gd
@@ -1,17 +1,32 @@
 extends Node2D
 
 @onready var _hand: Hand = $Hand
-@onready var _sequencer: Node2D = $Sequencer
-@onready var _background: Sprite2D = $Background
+@onready var _sequencer: Sequencer = $Sequencer
 @onready var _tray: Tray = $Tray
+@onready var _piano_roll: PianoRoll = $SkyLayer/PianoRoll
+@onready var _feedback: Node2D = $DetectionFeedback
+@onready var _background: Sprite2D = $Background
+
+const ORB_TRAIL_SCENE: PackedScene = preload("res://core/entities/orbs/orb_trail.tscn")
 
 func _ready() -> void:
 	_background.position = get_viewport_rect().size / 2
 	await get_tree().process_frame
-	_sequencer.setup(_tray)
+	_piano_roll.setup(_tray)
 	_sequencer.inject_hand(_hand)
 	_tray.connect_hand(_hand)
+	_sequencer.note_triggered.connect(_on_note_triggered)
 
-func _input(InputEvent) -> void:
+func _on_note_triggered(orb_id: Orb.OrbType, texture: Texture2D, from_position: Vector2, tick: int) -> void:
+	var target: Vector2 = _piano_roll.get_cell_position(tick, orb_id)
+	var trail := ORB_TRAIL_SCENE.instantiate() as OrbTrail
+	_feedback.add_child(trail)
+	trail.setup(texture, from_position, target)
+	var measure_duration: float = _sequencer.get_measure_duration()
+	trail.arrived.connect(func() -> void:
+		_piano_roll.receive_orb(orb_id, texture, tick, measure_duration)
+	)
+
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("dev_quit"):
 		get_tree().quit()
